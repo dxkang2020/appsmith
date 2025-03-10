6306abc7c0378d0f464dd9089e850ed8fd588cfb
du <du@runfox.com>
2025-03-10 06:11:52 +0000

    增加场景和卡片提示词

---
+++ pages/Page1/jsobjects/JSObject6/JSObject6.js
@@ -0,0 +1,14 @@
+export default {
+
+	getMarkdown() { return "```json\n" + this.test() + "\n```"; },
+	test(){
+		return JSON.stringify(Table1.selectedRow.script_json,null,2)
+	},
+	lines100(){
+		return Array.from({length: 100}, (_, i) => `${i+1}`).join("\n")
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
---
+++ pages/Page1/jsobjects/Markdown/Markdown.js
@@ -0,0 +1,18 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	getMarkdown(json) { return "```json\n" + JSON.stringify(json,null,2) + "\n```"; },
+	getScriptJson(){
+		return this.getMarkdown(Table1.selectedRow.script_json)
+	},
+	getScenePrompts(){
+		return  this.getMarkdown(Table1.selectedRow.scenes_prompt)
+	},
+	getCardPrompts(){
+		return  this.getMarkdown(Table1.selectedRow.cards_prompt)
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
