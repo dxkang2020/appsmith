e3efc112c10c7e7097542971677572ece7c73df7
du <du@runfox.com>
2025-03-18 08:31:51 +0000

    定稿

--- pages/Page1/jsobjects/soundList/soundList.js
+++ pages/Page1/jsobjects/soundList/soundList.js
@@ -7,40 +7,38 @@ export default {
 
 	jsonArr :Table1.selectedRow.script_json.scripts,
 	textArr:[
-  {
-    "step": "#1",
-    "task": "Drop a table",
-    "status": "approved"
-  },
-  {
-    "step": "#2",
-    "task": "Create a query fetch_users with the Mock DB",
-    "status": "pending"
-  },
-  {
-    "step": "#3",
-    "task": "Bind the query using => fetch_users.data",
-    "status": "pending"
-  }
-],
+		{
+			"step": "#1",
+			"task": "Drop a table",
+			"status": "approved"
+		},
+		{
+			"step": "#2",
+			"task": "Create a query fetch_users with the Mock DB",
+			"status": "pending"
+		},
+		{
+			"step": "#3",
+			"task": "Bind the query using => fetch_users.data",
+			"status": "pending"
+		}
+	],
 	getTexts(jsonArr){
+		const innerGetTexts = getTexts;
+		if (!Array.isArray(jsonArr)) {
+			jsonArr = [jsonArr];
+		}
 		let texts = new Set()
-		// var that = this
-		jsonArr.forEach(function(v){
-
+		jsonArr.forEach((v)=>{
 			if(v.text && v.type != "assess"){
 				texts.add(v.text)
 			}
-			if(v.feedback && v.feedback.length > 0)
-				v.feedback.forEach(function(v){
-					// texts.addAll(getTexts(v))
-					// that.getTexts([v]).foreach(function(text){
-					// texts.add(text)
-					// })
-					// texts.add(...getTexts(v))
+			if(v.feedback && v.feedback.length > 0){
+				v.feedback.forEach((item)=>{
+					texts.add(...getTexts([item]))
 				})
+			}
 		})
-
 		return texts
 	}
 
